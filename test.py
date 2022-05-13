a8ceb2a980e6e29b61eddc851e15c406228a7087
ChTole <ch.ariel.tole@gmail.com>
2022-05-13 15:14:07 -0300

    Agrego documentación

--- test.py
+++ test.py
@@ -3,6 +3,19 @@ import csv
 import json
 import datetime
 
+"""
+    Aplicación para consultas a la API de Mercado Libre:
+    El programa solicita el id numérico y ubicación de registro 
+    de uno o varios usuarios registrados en Mercado Libre.
+    Los datos inválidos no serán admitidos y deberán ingresarse nuevamente.
+    Luego, se emite un informe con formato a elección (.json, .csv, ambos 
+    o ninguno) que será descargado en la misma carpeta dónde se ejecute el programa.
+    El informe (indistinto el formato) contendrá los siguientes datos:
+    - ID del usuario
+    - Ítems publicados con sus correspondientes "id", "title" (título), 
+      "category_id" (id de categoría), "name_category" (nombre de la categoría).
+"""
+
 # Infomación correspondiente al nombre del archivo de salida
 def ahora():
     dia_hora = str(datetime.datetime.now())
